77e8bbaa50fd9fc0f116abf07a9d741736c83018
matthewmjm <matthewmjm@yahoo.com>
2021-03-24 18:52:36 -0600

    started coding tip calculator for day 2 project

--- day2/main.py
+++ day2/main.py
@@ -2,3 +2,6 @@ print("Welcome to the tip calculator!")
 total_bill = input("What was the total bill? ")
 tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
 party_size = input("How many people to split the bill? ")
+tip_amount = float(total_bill) * float(tip_percentage)
+grand_total_bill = float(total_bill) + tip_amount
+print(grand_total_bill)
