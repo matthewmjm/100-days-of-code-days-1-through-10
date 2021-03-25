17e523b2f1ed9615f02b657cca06653a760e6743
matthewmjm <matthewmjm@yahoo.com>
2021-03-24 18:41:19 -0600

    added day2 files

---
+++ day2/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the tip calculator!")
+total_bill = input("What was the total bill? ")
+tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
+party_size = input("How many people to split the bill? ")
