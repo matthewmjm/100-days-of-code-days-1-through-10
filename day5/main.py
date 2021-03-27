a8861aaa437e532f3b89fa63d6cc5473bb8d5264
matthewmjm <matthewmjm@yahoo.com>
2021-03-27 11:39:31 -0600

    finish password generator app

---
+++ day5/main.py
@@ -0,0 +1,34 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+password_list = []
+
+print("Welcome to the PyPassword Generator!")
+num_letters = int(input("How many letters would you like in your password?\n"))
+num_numbers = int(input("How many numbers would you like?\n"))
+num_symbols = int(input("How many symbols would you like?\n"))
+
+count = 0
+while count < num_letters:
+    count += 1
+    password_list.append(random.choice(letters))
+
+count = 0
+while count < num_numbers:
+    count += 1
+    password_list.append(random.choice(numbers))
+
+count = 0
+while count < num_symbols:
+    count += 1
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)    
+print(password_list)
+
+password = ""
+for ele in password_list:
+    password += ele
+print(f"Your password is: {password}")
