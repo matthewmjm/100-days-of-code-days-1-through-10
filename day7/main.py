dd53fabc2ad57fa8e13ed6579f010549481192c9
matthewmjm <matthewmjm@yahoo.com>
2021-03-29 22:57:59 -0600

    iterate through word to check guessed letter

--- day7/main.py
+++ day7/main.py
@@ -9,6 +9,13 @@ chosen_word = word_list[word_chose]
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
-print(guess)
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+guess_length = len(chosen_word)
+i = 0
+while i < guess_length:
+    if guess == chosen_word[i]:
+        print("Right")
+    else:
+        print("Wrong")
+    i += 1
