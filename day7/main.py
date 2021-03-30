2415729220f9f3e32dafc4f766588f469e728b53
matthewmjm <matthewmjm@yahoo.com>
2021-03-29 22:43:10 -0600

    ask user to chose a letter

--- day7/main.py
+++ day7/main.py
@@ -8,6 +8,7 @@ word_chose = random.randint(0, (size - 1))
 chosen_word = word_list[word_chose]
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-print(chosen_word)
+guess = input("Guess a letter: ").lower()
+print(guess)
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
