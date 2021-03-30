5e586617776ecae73b67d9139588e8cf87752c05
matthewmjm <matthewmjm@yahoo.com>
2021-03-29 23:10:48 -0600

    get each correct letter to appear in the list called display

--- day7/main.py
+++ day7/main.py
@@ -2,15 +2,21 @@ import random
 #Step 1 
 word_list = ["ardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#TODO-1.1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 size = len(word_list)
 word_chose = random.randint(0, (size - 1))
 chosen_word = word_list[word_chose]
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#TODO-1.2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#TODO-2.1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#TODO-1.3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#TODO-2.2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 guess_length = len(chosen_word)
 i = 0
 while i < guess_length:
@@ -19,3 +25,6 @@ while i < guess_length:
     else:
         print("Wrong")
     i += 1
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
