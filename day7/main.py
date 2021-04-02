eda603ab2859b483211886799c4e57622fdd7395
matthewmjm <matthewmjm@yahoo.com>
2021-04-01 20:42:41 -0600

    consider if user has won logic

--- day7/main.py
+++ day7/main.py
@@ -11,6 +11,10 @@ chosen_word = word_list[word_chose]
 #TODO-2.1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+#TODO-3.1: - Use a while loop to let the user guess again.  The loop should only stop once
+#the user has guessed all the letters in the chosen_word and 'display has no more blanks
+#("_") Then you can tell the user they've won.
+
 guess = input("Guess a letter: ").lower()
 i = 0
 display = []
